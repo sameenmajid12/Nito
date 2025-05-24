4c77eb55290cb02160432f59576fbffe0c1b914e
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-05-24 03:50:03 -0400

    Added app colors and font

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Pressable, StyleSheet, Text, View,Image, TextInput } from 'react-native';
+import colors from './constants/colors';
+import { useFonts } from 'expo-font';
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Nunito-Regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'Nunito-Bold': require('./assets/fonts/Nunito-Bold.ttf'),
+    'Nunito-SemiBold': require('./assets/fonts/Nunito-SemiBold.ttf'),
+    'Nunito-Medium': require('./assets/fonts/Nunito-Medium.ttf'),
+  });
+    if (!fontsLoaded) {
+    return null; 
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image  style={{ width: 150, height:64.5 }} source={require('./assets/images/logo.png')}></Image>
+      <Text style={{fontFamily:"Nunito-SemiBold", color:colors.black50, fontSize:20}}>Chat freely. Chat anonymously</Text>
+      <Pressable style={{width:"70%",height:45,borderRadius:10, backgroundColor:colors.primary, justifyContent:"center", alignItems:"center", shadowColor:"black", shadowOpacity:"0.25", shadowOffset:{ width:0, height:4}, shadowRadius:8}}><Text style={{color:"white", fontFamily:"Nunito-SemiBold"}}>Login</Text></Pressable>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.background,
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ constants/colors.js
@@ -0,0 +1,19 @@
+const colors = {
+  background:"#FDF0FF",
+  text:"#333",
+  black20:"#A49CA6",
+  black30:"#B6ACB8",
+  black40:"#989099",
+  black50:"#7F7880",
+  primary:"#7F68FF",
+  primary50:"#BEACFF",
+  primary70:"#A591FF",
+  accent:"#EA80FC",
+  accent40:"#F5C3FE",
+  accent70:"#F0A2FD",
+  blue:"#5B3FFC",
+  blue50:"#A690FF",
+  primaryDark:"#684EFF",
+  primaryDark70:"#957FFF"
+}
+export default colors;
