f4a3d257886e87c4f508e5b98696c4d378aef5ce
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-06-10 13:51:40 -0400

    Clean up

--- src/screens/main/ChatScreen.js
+++ src/screens/main/ChatScreen.js
@@ -1,16 +1,7 @@
 import { SafeAreaView, StyleSheet } from "react-native";
 import { colors, pageStyle } from "../../styles";
 function ChatScreen(){
-  console.log(pageStyle);
-  return(
-    <SafeAreaView style={styles.page}>
-    </SafeAreaView>
-  )
+  
 }
-const styles = StyleSheet.create({
-  page:{
-    backgroundColor:colors.background,
-    flex:1
-  }
-})
+
 export default ChatScreen;
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,3 @@
 export {default as colors} from './colors';
 
 export * from './typography';
-
-export * from './layout';
