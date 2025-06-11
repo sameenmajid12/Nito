97101c42367e4333a3cce440927903e770d21270
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-06-10 22:12:07 -0400

    Implement basic UI for chat screen

--- src/screens/main/ChatScreen.js
+++ src/screens/main/ChatScreen.js
@@ -1,7 +1,33 @@
-import { SafeAreaView, StyleSheet } from "react-native";
-import { colors, pageStyle } from "../../styles";
-function ChatScreen(){
-  
-}
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  View,
+} from "react-native";
+import { colors } from "../../styles";
+import MessageInput from "../../components/chat/MessageInput";
+import { useState } from "react";
+import ChatHeader from "../../components/chat/ChatHeader";
+function ChatScreen() {
+  const [message, setMessage] = useState("");
+  return (
+    <SafeAreaView style={styles.page}>
+      <ChatHeader/>
+      <ScrollView style={styles.mainChat}>
 
+      </ScrollView>
+      <MessageInput message={message} setMessage={setMessage}></MessageInput>
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  mainChat:{
+    flex:1,
+  },
+  
+});
 export default ChatScreen;
