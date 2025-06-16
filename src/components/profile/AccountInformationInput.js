f417221bfa2b66548087f84b526bdfdb780f7929
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-06-16 00:11:34 -0400

    Implement Account Information screen

--- src/components/profile/AccountInformationInput.js
+++ src/components/profile/AccountInformationInput.js
@@ -1,4 +1,71 @@
-function AccountInformationInput(){
-
+import { Ionicons } from "@expo/vector-icons";
+import { Text, TextInput, View, StyleSheet } from "react-native";
+import { colors, FONT_SIZE_L, FONT_SIZE_M, FONT_SIZE_XL } from "../../styles";
+function AccountInformationInput({
+  iconName,
+  label,
+  placeholder,
+  editable,
+  value,
+  setValue,
+  secure,
+}) {
+  return (
+    <View style={styles.mainContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <View style={styles.inputContainer}>
+        <Ionicons
+          style={[
+            styles.icon,
+            editable
+              ? { color: colors.textPrimary }
+              : { color: colors.textLight },
+          ]}
+          name={iconName}
+        ></Ionicons>
+        <TextInput
+          style={[
+            styles.input,
+            editable
+              ? { color: colors.textPrimary }
+              : { color: colors.textLight },
+          ]}
+          placeholder={placeholder}
+          editable={editable}
+          value={value}
+          setValue={setValue}
+          secureTextEntry={secure || false}
+        ></TextInput>
+      </View>
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  mainContainer: {
+    width: "100%",
+  },
+  inputContainer: {
+    borderWidth: 1,
+    borderColor: colors.borderLight,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 15,
+    borderRadius: 12,
+    columnGap: 7,
+  },
+  label: {
+    fontFamily: "Nunito-SemiBold",
+    color: colors.textPrimary,
+  },
+  icon: {
+    fontSize: FONT_SIZE_XL,
+    color: colors.textPrimary,
+    paddingVertical: 10,
+  },
+  input: {
+    flex: 1,
+    fontFamily: "Nunito-SemiBold",
+    height: "100%",
+  },
+});
 export default AccountInformationInput;
--- src/screens/main/Profile/ProfileAccountInformationScreen.js
+++ src/screens/main/Profile/ProfileAccountInformationScreen.js
@@ -1,8 +1,132 @@
-import { View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { colors, FONT_SIZE_L, FONT_SIZE_M, FONT_SIZE_S, FONT_SIZE_XL } from "../../../styles";
+import AccountInformationInput from "../../../components/profile/AccountInformationInput";
+import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
 
-function ProfileAccountInformationScreen(){
-  return(
-    <View></View>
-  )
+function ProfileAccountInformationScreen() {
+  const [edited, setEdited] = useState(false);
+  return (
+    <SafeAreaView style={styles.page}>
+      <View style={styles.header}>
+        <Ionicons
+          size={FONT_SIZE_L}
+          color={colors.textPrimary}
+          name="chevron-back"
+        ></Ionicons>
+        <Text style={styles.headerText}>Account Information</Text>
+      </View>
+      <View></View>
+      <View style={styles.contentContainer}>
+        <View>
+          <Text style={styles.pageHeader}>Details</Text>
+          <Text style={styles.pageSubheader}>Here you can edit your account information</Text>
+        </View>
+
+        <AccountInformationInput
+          iconName={"person-circle-outline"}
+          label={"Fullname"}
+          value={"Sameen Majid"}
+          placeholder={"Enter fullname"}
+          editable={true}
+        />
+        <AccountInformationInput
+          iconName={"at-outline"}
+          label={"Username"}
+          value={"user21454295"}
+          editable={false}
+        />
+        <AccountInformationInput
+          iconName={"mail-outline"}
+          label={"Email"}
+          value={"srm341@scarletmail.rutgers.edu"}
+          placeholder={"Enter email"}
+          editable={true}
+        />
+        <AccountInformationInput
+          iconName={"call-outline"}
+          label={"Phone number"}
+          value={"516-667-9552"}
+          placeholder={"Enter phone number"}
+          editable={true}
+        />
+        <AccountInformationInput
+          iconName={"shield-outline"}
+          label={"Password"}
+          value={"Samin123"}
+          placeholder={"Enter password"}
+          editable={true}
+          secure={true}
+        />
+        <TouchableOpacity
+          style={[styles.saveButton, edited?{backgroundColor:colors.primary}:{backgroundColor:colors.primary50}]}
+          disabled={edited}
+          activeOpacity={edited ? 0.9 : 1.0}
+        >
+          <Text style={styles.saveButtonText} disabled={edited}>
+            Save changes
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.borderLight,
+    columnGap: 5,
+    height: 41,
+  },
+  headerText: {
+    fontFamily: "Nunito-SemiBold",
+    fontSize: FONT_SIZE_M,
+  },
+  contentContainer: {
+    paddingVertical: 30,
+    paddingHorizontal: 30,
+    rowGap: 15,
+  },
+  saveButton: {
+    alignSelf: "flex-end",
+    width: 135,
+    height: 40,
+    borderRadius: 10,
+    marginTop: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  saveButtonText: {
+    color: colors.white,
+    fontFamily: "Nunito-Bold",
+    fontSize:FONT_SIZE_S
+  },
+  pageHeader:{
+    fontFamily:"Nunito-SemiBold",
+    fontSize:FONT_SIZE_XL,
+    textAlign:"center",
+    color:colors.textPrimary
+  },
+  pageSubheader:{
+    fontFamily:"Nunito-SemiBold",
+    fontSize:FONT_SIZE_S,
+    color:colors.textLight,
+    textAlign:"center",
+    marginBottom:15
+  }
+});
 export default ProfileAccountInformationScreen;
