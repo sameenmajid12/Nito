ccf070f366e1d030127d30dcf538a28804dcf25f
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-06-18 01:33:57 -0400

    Add AppNavigator component to condintionally render authentication and main navigator

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,21 @@
+import { useAuth } from "../contexts/AuthContext";
+import { useUser } from "../contexts/UserContext";
+import LoadingScreen from "../screens/LoadingScreen";
+import AuthNavigator from "./AuthNavigator";
+import MainNavigator from "./MainNavigator";
+
+function AppNavigator(){
+  const {isLoadingAuth, isAuthenticated} = useAuth();
+  const {isLoadingUser} = useUser();
+  console.log(isAuthenticated);
+  console.log(isLoadingAuth);
+  console.log(isLoadingUser);
+  if(isLoadingAuth || isLoadingUser){
+    return <LoadingScreen></LoadingScreen>
+  };
+  if(!isAuthenticated){
+    return <AuthNavigator/>
+  }
+  return <MainNavigator/>
+}
+export default AppNavigator;
