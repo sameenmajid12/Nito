103a312f9a41e2f7b7f959387e2cb20c4a7faffb
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-06-20 18:40:09 -0400

    Refactor RegistrationContext to enhance documentation and clarify registration data structure

--- src/contexts/RegistrationContext.js
+++ src/contexts/RegistrationContext.js
@@ -1,3 +1,28 @@
+/**
+ * RegistrationContext
+ *
+ * This context provides a global state management solution for the user registration process.
+ * It stores various registration data fields (e.g., school, fullname, email, password, image)
+ * across different screens of the registration flow. The idea is that in the first screeen
+ * the user will select their school(which will be a schoolObject with the properties of id, 
+ * name and emailDomain) this will be stored in registrationData using updateRegisterData. In 
+ * the next screen the user will enter more details such as their fullname,their email address, etc. 
+ * Again the information is stored using updateRegistrationData. In the final screen the user will
+ * type in tags(their interests) and after they are done will select the one of the two buttons. The 
+ * two buttons on the final screen are "skip" and "finish" and both of them call the REGISTER function
+ * from the authContext
+ *
+ * It exposes:
+ * - registrationData: An object containing all the collected registration information.
+ * - updateRegistrationData: A function to merge new data into the existing registrationData.
+ * - resetRegistration: A function to clear all registration data back to its initial state.
+ *
+ * Usage:
+ * Import { useRegistration } from './RegistrationContext';
+ * const { registrationData, updateRegistrationData } = useRegistration();
+ */
+
+
 import { createContext, useCallback, useContext, useState } from "react";
 
 const RegistrationContext = createContext();
@@ -6,7 +31,7 @@ const initialRegistrationState = {
   fullname: "",
   email: "",
   password:"",
-  schoolId: "",
+  school: "",
   tags: [],
   profilePic:null
 };
@@ -28,7 +53,6 @@ export const RegistrationProvider = ({ children }) => {
     registrationData,
     updateRegistrationData,
     resetRegistration,
-    initialRegistrationState
   };
   return (
     <RegistrationContext.Provider value={value}>
