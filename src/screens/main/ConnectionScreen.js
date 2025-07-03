a206522f2c2a4bebbeada70759c2647cb887a9e9
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-07-02 23:35:05 -0400

    Add keyboard dismiss on navigate back for connections screen

--- src/screens/main/ConnectionScreen.js
+++ src/screens/main/ConnectionScreen.js
@@ -4,27 +4,29 @@ import {
   StyleSheet,
   TextInput,
   View,
-  Text,
+  Keyboard
 } from "react-native";
-import {
-  colors,
-} from "../../styles";
+import { colors } from "../../styles";
 import TextHeader from "../../components/common/TextHeader";
-import ConnectionList from '../../components/common/ConnectionList';
+import ConnectionList from "../../components/common/ConnectionList";
 import SortConnection from "../../components/connection/SortConnection";
 import SearchConnection from "../../components/connection/SearchConnection";
+import { GestureDetector, Gesture } from "react-native-gesture-handler";
 function ConnectionScreen({ navigation }) {
+  const tapGesture = Gesture.Tap().onTouchesDown(() => Keyboard.dismiss());
   return (
     <SafeAreaView style={styles.page}>
       <TextHeader navigation={navigation} text={"Connections"} />
-      <ScrollView>
-        <View style={styles.mainContainer}>
-          <SearchConnection/>
-          <View style={styles.divider}></View>
-          <SortConnection/>
-          <ConnectionList connections={['s','s','s']} gap={15}/>
-        </View>
-      </ScrollView>
+      <GestureDetector gesture={tapGesture}>
+        <ScrollView>
+          <View style={styles.mainContainer}>
+            <SearchConnection />
+            <View style={styles.divider}></View>
+            <SortConnection />
+            <ConnectionList connections={["s", "s", "s"]} gap={15} />
+          </View>
+        </ScrollView>
+      </GestureDetector>
     </SafeAreaView>
   );
 }
@@ -35,19 +37,19 @@ const styles = StyleSheet.create({
   },
   mainContainer: {
     padding: 30,
-    rowGap:20,
-    marginBottom:60
+    rowGap: 20,
+    marginBottom: 60,
   },
-  
+
   divider: {
     width: "100%",
     height: 1,
     borderTopWidth: 1,
     borderTopColor: colors.borderLight,
   },
-  
-  connectionsContainer:{
-    rowGap:15
-  }
+
+  connectionsContainer: {
+    rowGap: 15,
+  },
 });
 export default ConnectionScreen;
