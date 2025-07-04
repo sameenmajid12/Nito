d2fee63591f3ff5ce85366695dd03b8a5a734d85
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-07-04 00:37:54 -0400

    Made sort label dynamic

--- src/components/connection/SortConnection.js
+++ src/components/connection/SortConnection.js
@@ -2,12 +2,12 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { FONT_SIZE_XL, FONT_SIZE_M, colors } from "../../styles";
 import { useModal } from "../../contexts/ModalContext";
-import { useEffect } from "react";
 function SortConnection({ sortState, setSortState }) {
   const { openModal } = useModal();
   const changeSort = (newSort) => {
     setSortState(newSort);
   };
+  const sortLabel = sortState === "newestfirst" ? "Default" : sortState === "oldestfirst" ? "Oldest first" : "A-Z"
   return (
     <View style={styles.sortContainer}>
       <View style={styles.sortContainerSections}>
@@ -19,7 +19,7 @@ function SortConnection({ sortState, setSortState }) {
         style={styles.sortContainerSections}
       >
         <Text style={styles.sortText}>Sort by</Text>
-        <Text style={styles.sortSelectedValue}>Default</Text>
+        <Text style={styles.sortSelectedValue}>{sortLabel}</Text>
         <Ionicons
           color={colors.primary}
           size={FONT_SIZE_M}
