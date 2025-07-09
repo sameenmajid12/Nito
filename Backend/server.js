0e2d02948abd1d9a6fb278855062ec47fab54fc9
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-07-09 02:54:36 -0400

    Update CORS configuration to allow all origins and add school router endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const http = require("http");
 const { authRouter } = require("./controllers/authController.js");
+const { schoolRouter } = require("./controllers/schoolController.js");
 const connectDB = require("./config/db.js");
 
 const corsOptions = {
-  origin: "http://192.168.1.173:5173",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 };
@@ -17,8 +19,11 @@ const app = express();
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use("/auth", authRouter);
+app.use("/school", schoolRouter);
 const server = http.createServer(app);
-
+app.get("/", async (req, res, next) => {
+  res.json({ message: "Hello from backend" });
+});
 connectDB().then(() => {
   server.listen(port, () => {
     console.log(`Connected to 192.168.1.173:${port}`);
