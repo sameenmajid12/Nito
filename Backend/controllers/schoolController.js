de98c272f2c4b7f951e81704bc20a735b7f6fd0c
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-07-09 02:57:02 -0400

    Add endpoint to retrieve all school data

---
+++ Backend/controllers/schoolController.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const schoolRouter = express.Router();
+const School = require("../models/SchoolModel");
+
+schoolRouter.get("/", async(req,res,next)=>{
+  try{
+    const schools = await School.find({});
+    if(!schools){
+      return res.status(404).json({message:"Error fetching schools"})
+    }
+    res.status(200).json({schools});
+  }
+  catch(error){
+
+  }
+})
+
+module.exports = { schoolRouter };
--- Backend/models/SchoolModel.js
+++ Backend/models/SchoolModel.js
@@ -10,6 +10,10 @@ const schoolSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  image:{
+    type:String,
+    required:true
+  }
 });
 
 module.exports = mongoose.model("School", schoolSchema);
