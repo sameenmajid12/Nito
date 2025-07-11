c68a5c9513710a977f17edb6c14534baae256298
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-07-11 13:41:44 -0400

    Refactor verifyToken middleware to exit the function on a bad call

--- Backend/middleware/verifyToken.js
+++ Backend/middleware/verifyToken.js
@@ -2,16 +2,18 @@ const jwt = require("jsonwebtoken");
 
 
 function verifyToken(req,res,next){
+  console.log("Verifying token...")
   const authHeader = req.headers.authorization;
+  console.log(authHeader);
   if(!authHeader || !authHeader.startsWith("Bearer ")){
-    res.status(401).json({message:"Access token missing or malformed"});
+    return res.status(401).json({message:"Access token missing or malformed"});
   }
 
   const token = authHeader.split(" ")[1];
-
+ 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
     if(err){
-      res.status(403).json({message:"Invalid or expired token"});
+      return res.status(403).json({message:"Invalid or expired token"});
     }
     req.user = decoded
     next();
