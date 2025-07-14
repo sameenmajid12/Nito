0fb2bca961bb1c87dd4e79e9ddf817e7bd7ae1f2
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-07-14 16:33:13 -0400

    Implement patch route for updating user profile picture

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -3,6 +3,9 @@ const userRouter = express.Router();
 const User = require("../models/UserModel");
 const verifyToken = require("../middleware/verifyToken");
 const bcrypt = require("bcrypt");
+const upload = require("../middleware/uploadImage");
+const s3 = require("../config/s3Client");
+const { PutObjectCommand } = require("@aws-sdk/client-s3");
 userRouter.get("/me", verifyToken, async (req, res) => {
   try {
     console.log("Getting user...");
@@ -60,5 +63,36 @@ userRouter.patch("/remove-connection", verifyToken, async (req, res, next) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
+userRouter.patch(
+  "/update-profilePic",
+  verifyToken,
+  upload.single("profilePic"),
+  async (req, res) => {
+    try {
+      console.log("Updating profile picture");
+      if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+      }
+      const fileUploadParams = {
+        Bucket: "nito-s3-image-bucket",
+        Key: req.file.originalname,
+        Body: req.file.buffer,
+        ContentType: req.file.mimetype,
+      };
+      await s3.send(new PutObjectCommand(fileUploadParams));
+      const newFileUrl = `https://${fileUploadParams.Bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${req.file.originalname}`;
+      const user = await User.findById(req.user._id);
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      user.profilePic = newFileUrl;
+      await user.save();
+      res.status(200).json({ profilePic: newFileUrl });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
 
 module.exports = { userRouter };
