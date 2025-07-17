a0f162bff2ef901a774f2e68f5fe8985a9648fd7
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-07-16 23:42:51 -0400

    Create useMessages hook for conversation message handling

---
+++ Frontend/src/hooks/useMessages.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useEffect, useState, useCallback } from "react";
+import { API_BASE_URL } from "@env";
+import { useAuth } from "../contexts/AuthContext";
+
+function useMessages(conversationId) {
+  const { token } = useAuth();
+  const [messages, setMessages] = useState(null);
+  const [isLoadingMessages, setIsLoadingMessage] = useState(false);
+
+  const getMessages = useCallback(async () => {
+    if (!conversationId) return;
+    setIsLoadingMessage(true);
+    try {
+      const response = await axios.get(
+        `${API_BASE_URL}/${conversationId}/messages`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (response.status === 200) {
+        const { conversationMessages } = response.data;
+        setMessages(conversationMessages);
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoadingMessage(false);
+    }
+  }, [conversationId, token]);
+
+  useEffect(() => {
+    getMessages();
+    return () => setMessages(null);
+  }, [getMessages]);
+
+  return { messages, isLoadingMessages };
+}
+
+export default useMessages;
