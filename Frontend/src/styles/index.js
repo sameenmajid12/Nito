7ddcb96a757974a530a78dbdd2d69577648057fa
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-07-18 20:00:00 -0400

    Add style exports for consistency and clarity

--- Frontend/src/styles/index.js
+++ Frontend/src/styles/index.js
@@ -1,3 +1,6 @@
-export {default as colors} from './colors';
+export { default as colors } from "./colors";
 
-export * from './typography';
+export * from "./typography";
+const PRIMARY_ACTIVE_OPACITY = 0.75;
+const TEXT_ACTIVE_OPACITY = 0.5;
+export { PRIMARY_ACTIVE_OPACITY, TEXT_ACTIVE_OPACITY };
