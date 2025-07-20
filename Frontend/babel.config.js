7465fb27a9468d50beecacf392ad1a3b776ec36c
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-07-19 23:51:53 -0400

    Update dependencies

--- Frontend/babel.config.js
+++ Frontend/babel.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['babel-preset-expo'],
   plugins: [
     ['module:react-native-dotenv', {
       moduleName: '@env',
       path: '.env',
       allowUndefined: true,
-    }]
+    }],
+    ['@babel/plugin-transform-private-methods', { loose: true }]
   ],
 };
