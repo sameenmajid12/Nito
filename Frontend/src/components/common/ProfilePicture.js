6e76584341bb0a4a56ac1ca9be1510c229694c44
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-08-11 19:01:56 -0400

    Fix bug where profile picture during auth dissapear after image upload cancel

--- Frontend/src/components/common/ProfilePicture.js
+++ Frontend/src/components/common/ProfilePicture.js
@@ -13,9 +13,11 @@ import * as ImageManipulator from "expo-image-manipulator";
 import { useState } from "react";
 import { useUser } from "../../contexts/UserContext";
 import ErrorMessage from "./ErrorMessage";
+import { useRegistration } from "../../contexts/RegistrationContext";
 function ProfilePicture({ image, setImage, handleConfirm, type, error }) {
   const [isLoadingImage, setIsLoadingImage] = useState(false);
   const { user } = useUser();
+  const {registrationData} = useRegistration();
   const pickProfilePic = async () => {
     setIsLoadingImage(true);
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -69,7 +71,7 @@ function ProfilePicture({ image, setImage, handleConfirm, type, error }) {
       }
     } else {
       if (type === "auth") {
-        setImage(null);
+        setImage(registrationData.profilePic.uri);
       } else {
         setImage(user.profilePic);
       }
