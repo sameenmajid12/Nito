a70fad3a4525e9805c76f979c445ae085d44d9a4
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-08-12 18:48:26 -0400

    Add empty state text to connections chat

--- Frontend/src/components/chat/ConnectionChats.js
+++ Frontend/src/components/chat/ConnectionChats.js
@@ -1,8 +1,8 @@
 import { StyleSheet, View, Pressable, Text } from "react-native";
-import { Image } from "expo-image";
 import { useUser } from "../../contexts/UserContext";
 import { colors, FONT_SIZE_S, FONT_SIZE_M, FONT_SIZE_XS } from "../../styles";
 import { formatLastMessageTime } from "../../utils/Format";
+import { Image } from "expo-image";
 function ConnectionChats({ navigation }) {
   const { user } = useUser();
   function truncateMessage(message, maxLength, name) {
@@ -15,66 +15,87 @@ function ConnectionChats({ navigation }) {
   return (
     <View style={styles.conversationListContainer}>
       <Text style={styles.containerHeader}>Your connections</Text>
-      {user?.savedConversations?.map((conversation, index) => {
-        const userNum = conversation?.user1?._id === user._id ? "user1" : "user2";
-        const isRead =
-          conversation.lastReadMessages[userNum]?._id === conversation.lastMessage?._id ||
-          conversation.lastMessage?.sender === user._id;
-        const otherUser = conversation.user1?._id === user._id ? conversation.user2 : conversation.user1;
-        return (
-          <Pressable
-            onPress={() => navigation.navigate("Chat", { conversation })}
-            key={index}
-            style={({ pressed }) => [
-              styles.conversation,
-              index === 0 && styles.topconversation,
-              pressed
-                ? {
-                    backgroundColor: colors.black5,
-                    borderBottomColor: colors.borderHover,
-                    borderTopColor: index === 0 ? colors.borderHover : null,
-                  }
-                : {
-                    backgroundColor: colors.background,
-                    borderBottomColor: colors.borderLight,
-                  },
-            ]}
-          >
-            <Image
-              style={styles.conversationProfilePic}
-              source={otherUser.profilePic}
-            ></Image>
-            <View style={styles.conversationDetails}>
-              <Text style={styles.conversationName}>{otherUser.fullname}</Text>
-              <Text
-                style={[
-                  styles.conversationLastMessage,
-                  isRead
-                    ? { color: colors.textLight, fontFamily: "Nunito-Medium" }
-                    : {
-                        color: colors.textPrimary,
-                        fontFamily: "Nunito-SemiBold",
-                      },
-                ]}
-              >
-                {truncateMessage(conversation.lastMessage, 60, otherUser.fullname)}
-              </Text>
-            </View>
-            <Text
-              style={[
-                styles.conversationLastMessageTime,
-                isRead
-                  ? { color: colors.textLight }
-                  : { color: colors.textPrimary },
+      {user.savedConversations?.length > 0 ? (
+        user?.savedConversations?.map((conversation, index) => {
+          const userNum =
+            conversation?.user1?._id === user._id ? "user1" : "user2";
+          const isRead =
+            conversation.lastReadMessages[userNum]?._id ===
+              conversation.lastMessage?._id ||
+            conversation.lastMessage?.sender === user._id;
+          const otherUser =
+            conversation.user1?._id === user._id
+              ? conversation.user2
+              : conversation.user1;
+          return (
+            <Pressable
+              onPress={() => navigation.navigate("Chat", { conversation })}
+              key={index}
+              style={({ pressed }) => [
+                styles.conversation,
+                index === 0 && styles.topconversation,
+                pressed
+                  ? {
+                      backgroundColor: colors.black5,
+                      borderBottomColor: colors.borderHover,
+                      borderTopColor: index === 0 ? colors.borderHover : null,
+                    }
+                  : {
+                      backgroundColor: colors.background,
+                      borderBottomColor: colors.borderLight,
+                    },
               ]}
             >
-              {conversation.lastMessage &&
-                formatLastMessageTime(new Date(conversation.lastMessage.createdAt))}
-            </Text>
-            {!isRead && <View style={styles.unreadIndicator}></View>}
-          </Pressable>
-        );
-      })}
+              <Image
+                style={styles.conversationProfilePic}
+                source={otherUser.profilePic}
+              ></Image>
+              <View style={styles.conversationDetails}>
+                <Text style={styles.conversationName}>
+                  {otherUser.fullname}
+                </Text>
+                <Text
+                  style={[
+                    styles.conversationLastMessage,
+                    isRead
+                      ? { color: colors.textLight, fontFamily: "Nunito-Medium" }
+                      : {
+                          color: colors.textPrimary,
+                          fontFamily: "Nunito-SemiBold",
+                        },
+                  ]}
+                >
+                  {truncateMessage(
+                    conversation.lastMessage,
+                    60,
+                    otherUser.fullname
+                  )}
+                </Text>
+              </View>
+              <Text
+                style={[
+                  styles.conversationLastMessageTime,
+                  isRead
+                    ? { color: colors.textLight }
+                    : { color: colors.textPrimary },
+                ]}
+              >
+                {conversation.lastMessage &&
+                  formatLastMessageTime(
+                    new Date(conversation.lastMessage.createdAt)
+                  )}
+              </Text>
+              {!isRead && <View style={styles.unreadIndicator}></View>}
+            </Pressable>
+          );
+        })
+      ) : (
+        <View style={styles.emptyStateWrapper}>
+          <Text style={styles.emptyStateText}>
+            Chats will appear here once you have other users as connections
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -135,6 +156,17 @@ const styles = StyleSheet.create({
     position: "absolute",
     right: 10,
     top: 37,
+  },
+  emptyStateWrapper: {
+    borderTopWidth: 1,
+    borderTopColor: colors.borderLight,
+    paddingHorizontal: 20,
+    paddingTop:10
+  },
+  emptyStateText: {
+    fontFamily: "Nunito-SemiBold",
+    fontSize: FONT_SIZE_S,
+    color: colors.textPrimary,
   },
 });
 export default ConnectionChats;
