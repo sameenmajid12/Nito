eec5c5f09b0a3dcee2e46c0f63113bc57039c394
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-08-17 03:25:08 -0400

    Move message handling into useMessages hook

--- Frontend/src/hooks/useMessages.js
+++ Frontend/src/hooks/useMessages.js
@@ -1,41 +1,154 @@
 import axios from "axios";
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 import { API_BASE_URL } from "@env";
 import { useAuth } from "../contexts/AuthContext";
-
-function useMessages(conversationId) {
+import { useUser } from "../contexts/UserContext";
+import { useSocket } from "../contexts/SocketContext";
+import { v4 as uuidv4 } from "uuid";
+function useMessages(conversation) {
   const { token } = useAuth();
-  const [messages, setMessages] = useState(null);
+  const { user, setUser } = useUser();
+  const { socket } = useSocket();
+  const [messages, setMessages] = useState([]);
   const [isLoadingMessages, setIsLoadingMessage] = useState(false);
-
-  const getMessages = useCallback(async () => {
-    if (!conversationId) return;
+  const [newMessage, setNewMessage] = useState("");
+  const [hasLoadedMessages, setHasLoadedMessages] = useState(false);
+  const otherUser =
+    conversation.user1._id === user._id
+      ? conversation.user2
+      : conversation.user1;
+  const getMessages = async () => {
     setIsLoadingMessage(true);
     try {
       const response = await axios.get(
-        `${API_BASE_URL}/${conversationId}/messages`,
+        `${API_BASE_URL}/conversation/${conversation._id}/messages`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         }
       );
+      if (response.status === 200) {
         const { conversationMessages } = response.data;
         setMessages(conversationMessages);
-      
+      }
     } catch (e) {
       console.error(e);
     } finally {
       setIsLoadingMessage(false);
+      setHasLoadedMessages(true);
     }
-  }, [conversationId, token]);
+  };
 
   useEffect(() => {
-    getMessages();
-    return () => setMessages(null);
-  }, [getMessages]);
+    if (conversation && !hasLoadedMessages) {
+      getMessages();
+    }
+  }, [conversation, hasLoadedMessages]);
 
-  return { messages, isLoadingMessages };
+  useEffect(() => {
+    const userNum = user._id === conversation.user1._id ? "user1" : "user2";
+    const data = {
+      conversationId: conversation._id,
+      receiverId: otherUser._id,
+    };
+    if (
+      conversation.lastMessage?._id !==
+        conversation.lastReadMessages[userNum]?._id &&
+      conversation.lastMessage?.sender !== user._id
+    ) {
+      socket.emit("markConversationAsRead", data);
+      setUser((prev) => {
+        if (conversation._id === user.currentConversation?._id) {
+          return {
+            ...prev,
+            currentConversation: {
+              ...prev.currentConversation,
+              lastReadMessages: {
+                ...prev.currentConversation?.lastReadMessages,
+                [userNum]: conversation.lastMessage,
+              },
+            },
+          };
+        }
+        const conversationIndex = prev.savedConversations.findIndex(
+          (convo) => convo._id === conversation._id
+        );
+        if (conversationIndex === -1) return prev;
+        const updatedConversation = {
+          ...prev.savedConversations[conversationIndex],
+          lastReadMessages: {
+            ...prev.savedConversations[conversationIndex].lastReadMessages,
+            [userNum]: conversation.lastMessage,
+          },
+        };
+        const newConversations = [...prev.savedConversations];
+        newConversations[conversationIndex] = updatedConversation;
+        return { ...prev, savedConversations: newConversations };
+      });
+    }
+  }, [messages, conversation]);
+
+  useEffect(() => {
+    if (!socket) return;
+    const handleReceiveMessage = (message) => {
+      if (message.conversation !== conversation._id) {
+        return;
+      }
+      console.log("Received new message for current chat");
+
+      setMessages((prevMessages) => {
+        if (prevMessages.some((m) => m._id === message._id)) {
+          return prevMessages;
+        }
+        const indexToReplace = prevMessages.findIndex(
+          (m) => m._id === message.clientId
+        );
+        if (indexToReplace !== -1) {
+          const newMessages = [...prevMessages];
+          newMessages[indexToReplace] = message;
+          return newMessages;
+        } else {
+          return [...prevMessages, message];
+        }
+      });
+    };
+    socket.on("receiveMessage", handleReceiveMessage);
+    return () => {
+      socket.off("receiveMessage", handleReceiveMessage);
+    };
+  }, [socket, conversation]);
+
+  const sendMessage = () => {
+    if (!newMessage.trim() || !socket || !user?._id) {
+      return;
+    }
+    const clientId = uuidv4();
+    const messageToSend = {
+      receiverId: otherUser._id,
+      conversationId: conversation._id,
+      text: newMessage.trim(),
+      clientId,
+    };
+    const tempMessage = {
+      _id: clientId,
+      sender: user._id,
+      receiver: otherUser._id,
+      conversation: conversation._id,
+      text: newMessage.trim(),
+    };
+    socket.emit("sendMessage", messageToSend);
+    setMessages((prevMessages) => [...prevMessages, tempMessage]);
+    setNewMessage("");
+  };
+
+  return {
+    messages,
+    isLoadingMessages,
+    setNewMessage,
+    newMessage,
+    sendMessage,
+  };
 }
 
 export default useMessages;
--- Frontend/src/screens/main/Chat/ChatScreen.js
+++ Frontend/src/screens/main/Chat/ChatScreen.js
@@ -12,20 +12,17 @@ import ChatHeader from "../../../components/chat/ChatHeader";
 import MessagesContainer from "../../../components/chat/MessagesContainer";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import { useUser } from "../../../contexts/UserContext";
-import { API_BASE_URL } from "@env";
-import { useAuth } from "../../../contexts/AuthContext";
-import axios from "axios";
-import { v4 as uuidv4 } from "uuid";
-import { useSocket } from "../../../contexts/SocketContext";
+import useMessages from "../../../hooks/useMessages";
 function ChatScreen({ navigation, route }) {
-  const [newMessage, setNewMessage] = useState("");
-  const { user, setUser } = useUser();
-  const { token } = useAuth();
+  const { user } = useUser();
   const [conversation, setConversation] = useState(route.params.conversation);
-  const { socket } = useSocket();
-  const [messages, setMessages] = useState([]);
-  const [loadingMessages, setLoadingMessages] = useState(false);
-  const [hasLoadedMessages, setHasLoadedMessages] = useState(false);
+  const {
+    messages,
+    isLoadingMessages,
+    setNewMessage,
+    newMessage,
+    sendMessage,
+  } = useMessages(conversation);
   const [showTime, setShowTime] = useState(false);
   const toggleTime = () => {
     setShowTime((prev) => !prev);
@@ -53,33 +50,7 @@ function ChatScreen({ navigation, route }) {
     conversation.user1._id === user._id
       ? conversation.user2
       : conversation.user1;
-  useEffect(() => {
-    const getMessages = async () => {
-      setLoadingMessages(true);
-      try {
-        const response = await axios.get(
-          `${API_BASE_URL}/conversation/${conversation._id}/messages`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        if (response.status === 200) {
-          const { conversationMessages } = response.data;
-          setMessages(conversationMessages);
-        }
-      } catch (e) {
-        console.error(e);
-      } finally {
-        setLoadingMessages(false);
-        setHasLoadedMessages(true);
-      }
-    };
-    if (conversation && !hasLoadedMessages) {
-      getMessages();
-    }
-  }, [conversation]);
+
   useEffect(() => {
     if (messages.length > 0) {
       setConversation((prev) => ({
@@ -88,99 +59,7 @@ function ChatScreen({ navigation, route }) {
       }));
     }
   }, [messages]);
-  useEffect(() => {
-    const userNum = user._id === conversation.user1._id ? "user1" : "user2";
-    const data = {
-      conversationId: conversation._id,
-      receiverId: otherUser._id,
-    };
-    if (
-      conversation.lastMessage?._id !==
-        conversation.lastReadMessages[userNum]?._id &&
-      conversation.lastMessage?.sender !== user._id
-    ) {
-      socket.emit("markConversationAsRead", data);
-      setUser((prev) => {
-        if (conversation._id === user.currentConversation?._id) {
-          return {
-            ...prev,
-            currentConversation: {
-              ...prev.currentConversation,
-              lastReadMessages: {
-                ...prev.currentConversation?.lastReadMessages,
-                [userNum]: conversation.lastMessage,
-              },
-            },
-          };
-        }
-        const conversationIndex = prev.savedConversations.findIndex(
-          (convo) => convo._id === conversation._id
-        );
-        if (conversationIndex === -1) return prev;
-        const updatedConversation = {
-          ...prev.savedConversations[conversationIndex],
-          lastReadMessages: {
-            ...prev.savedConversations[conversationIndex].lastReadMessages,
-            [userNum]: conversation.lastMessage,
-          },
-        };
-        const newConversations = [...prev.savedConversations];
-        newConversations[conversationIndex] = updatedConversation;
-        return { ...prev, savedConversations: newConversations };
-      });
-    }
-  }, [messages, conversation]);
-  useEffect(() => {
-    if (!socket) return;
-    const handleReceiveMessage = (message) => {
-      if (message.conversation !== conversation._id) {
-        return;
-      }
-      console.log("Received new message for current chat");
 
-      setMessages((prevMessages) => {
-        if (prevMessages.some((m) => m._id === message._id)) {
-          return prevMessages;
-        }
-        const indexToReplace = prevMessages.findIndex(
-          (m) => m._id === message.clientId
-        );
-        if (indexToReplace !== -1) {
-          const newMessages = [...prevMessages];
-          newMessages[indexToReplace] = message;
-          return newMessages;
-        } else {
-          return [...prevMessages, message];
-        }
-      });
-    };
-    socket.on("receiveMessage", handleReceiveMessage);
-    return () => {
-      socket.off("receiveMessage", handleReceiveMessage);
-    };
-  }, [socket, conversation]);
-  const sendMessage = () => {
-    if (!newMessage.trim() || !socket || !user?._id) {
-      return;
-    }
-    const clientId = uuidv4();
-    const messageToSend = {
-      receiverId: otherUser._id,
-      conversationId: conversation._id,
-      text: newMessage.trim(),
-      clientId,
-    };
-    const tempMessage = {
-      _id: clientId,
-      sender: user._id,
-      receiver: otherUser._id,
-      conversation: conversation._id,
-      text: newMessage.trim(),
-    };
-    socket.emit("sendMessage", messageToSend);
-    setMessages((prevMessages) => [...prevMessages, tempMessage]);
-    setNewMessage("");
-  };
   return (
     <SafeAreaView style={styles.page}>
       <ChatHeader
@@ -192,10 +71,12 @@ function ChatScreen({ navigation, route }) {
         showTime={showTime}
         toggleTime={toggleTime}
       />
-      <View style={{ flex: 1, flexDirection:"row" }}>
-        <GestureDetector gesture={sideGesture}><View style={{width:25}}></View></GestureDetector>
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <GestureDetector gesture={sideGesture}>
+          <View style={{ width: 25 }}></View>
+        </GestureDetector>
         <GestureDetector gesture={mainGesture}>
-          {!loadingMessages ? (
+          {!isLoadingMessages ? (
             <MessagesContainer
               messages={messages}
               conversation={conversation}
