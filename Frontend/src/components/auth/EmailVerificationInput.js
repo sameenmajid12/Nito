1e77af889ab75b3b69bfffb3a2220d9252692056
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-08-22 01:10:23 -0400

    Create email verification input component for enhanced reusability

---
+++ Frontend/src/components/auth/EmailVerificationInput.js
@@ -0,0 +1,68 @@
+import { StyleSheet, View, TextInput } from "react-native";
+import ErrorMessage from "../common/ErrorMessage";
+import { useRef } from "react";
+import { colors, FONT_SIZE_L } from "../../styles";
+function EmailVerificationInput({ code, setCode, error, inputSize }) {
+  const inputRefs = useRef([]);
+  const changeText = (num, index) => {
+    if (num.length <= 1) {
+      setCode((prev) => {
+        const newCode = [...prev];
+        newCode[index] = num;
+        return newCode;
+      });
+      if (index < 4 && num) {
+        inputRefs.current[index + 1]?.focus();
+      }
+    }
+  };
+  const handleKeyPress = (e, index) => {
+    if (e.nativeEvent.key === "Backspace" && index > 0) {
+      inputRefs.current[index - 1]?.focus();
+    }
+  };
+  return (
+    <View>
+      <View style={styles.inputWrapper}>
+        {[...Array(5)].map((_, index) => {
+          return (
+            <TextInput
+              ref={(el) => (inputRefs.current[index] = el)}
+              key={index}
+              keyboardType="numeric"
+              style={[
+                styles.input,
+                { borderColor: error ? "red" : colors.borderLight },
+                {width:inputSize, height:inputSize}
+              ]}
+              value={code[index]}
+              onKeyPress={(e) => handleKeyPress(e, index)}
+              onChangeText={(text) => changeText(text, index)}
+              maxLength={1}
+            ></TextInput>
+          );
+        })}
+      </View>
+      {error && (
+        <ErrorMessage message={error} style={{ marginTop: 5 }}></ErrorMessage>
+      )}
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  inputWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  input:{
+    borderWidth: 1,
+    borderColor: colors.borderLight,
+    borderRadius: 10,
+    textAlign: "center",
+    color: colors.textPrimary,
+    fontFamily: "Nunito-Medium",
+    fontSize: FONT_SIZE_L,
+  }
+});
+export default EmailVerificationInput;
