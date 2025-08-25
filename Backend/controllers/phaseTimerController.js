e41412ea7edf8535e5e4c23bea595cb9e2c10c6e
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-08-25 02:03:29 -0400

    Remove debug log

--- Backend/controllers/phaseTimerController.js
+++ Backend/controllers/phaseTimerController.js
@@ -6,7 +6,6 @@ phaseTimerRouter.get("/", async (req, res) => {
   try {
     console.log("Getting updated phase times");
     const times = await PhaseTimer.findOne({key:"globalTimers"});
-    console.log(times);
     if (!times) {
       return res.status(404).json({ message: "App state times not found" });
     }
