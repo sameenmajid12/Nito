6114e34b7dabadc5eb5f42e15ef55407677d178c
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-08-26 15:38:32 -0400

    Implemnet nextPair countdown for empty state message in conenction list

--- Frontend/src/components/common/ConnectionList.js
+++ Frontend/src/components/common/ConnectionList.js
@@ -2,11 +2,15 @@ import { Ionicons } from "@expo/vector-icons";
 import { colors, FONT_SIZE_M, TEXT_ACTIVE_OPACITY } from "../../styles";
 import ConnectionItem from "./ConnectionItem";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { usePhaseTimer } from "../../contexts/PhaseTimerContext";
+import { useUser } from "../../contexts/UserContext";
 function ConnectionList({ screen, connections, navigation }) {
+  const { user } = useUser();
+  const { countdowns } = usePhaseTimer();
   return (
     <View>
-      {connections.length > 0 ? (
-        connections.map((connection, index) => (
+      {connections?.length > 0 ? (
+        connections?.map((connection, index) => (
           <ConnectionItem
             key={index}
             connection={connection}
@@ -17,13 +21,29 @@ function ConnectionList({ screen, connections, navigation }) {
       ) : (
         <View style={styles.emptyStateWrapper}>
           <Text style={styles.emptyStateText}>
-            It's a little quiet here... start texting and make some connections!
-            🤝
+            {user.currentConversation
+              ? "It's a little quiet here... start texting and make a \nconnection"
+              : `No connections yet... your next pair will appear in ${countdowns.untilNextPair}`}
+            ! 🤝
           </Text>
-          <TouchableOpacity activeOpacity={TEXT_ACTIVE_OPACITY} style={styles.emptyStateButton}>
-            <Text style={styles.emptyStateButtonText}>Chat now</Text>
-            <Ionicons color={colors.primaryDark} size={FONT_SIZE_M} name="arrow-forward"></Ionicons>
-          </TouchableOpacity>
+          {user.currentConversation && (
+            <TouchableOpacity
+              activeOpacity={TEXT_ACTIVE_OPACITY}
+              style={styles.emptyStateButton}
+              onPress={() =>
+                navigation.navigate("Chat", {
+                  conversation: user.currentConversation,
+                })
+              }
+            >
+              <Text style={styles.emptyStateButtonText}>Chat now</Text>
+              <Ionicons
+                color={colors.primaryDark}
+                size={FONT_SIZE_M}
+                name="arrow-forward"
+              ></Ionicons>
+            </TouchableOpacity>
+          )}
         </View>
       )}
     </View>
@@ -31,27 +51,27 @@ function ConnectionList({ screen, connections, navigation }) {
 }
 
 const styles = StyleSheet.create({
-  emptyStateWrapper:{
-    justifyContent:"center",
-    paddingHorizontal:20,
-    paddingVertical:30,
-    rowGap:20
+  emptyStateWrapper: {
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    rowGap: 20,
   },
-  emptyStateText:{
-    fontFamily:"Nunito-SemiBold",
-    textAlign:"center",
+  emptyStateText: {
+    fontFamily: "Nunito-SemiBold",
+    textAlign: "center",
   },
-  emptyStateButton:{
-    flexDirection:'row',
-    justifyContent:'center',
-    alignItems:"center",
-    columnGap:2
+  emptyStateButton: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    columnGap: 2,
   },
-  emptyStateButtonText:{
-    textAlign:"center",
-    fontFamily:"Nunito-SemiBold",
-    color:colors.primaryDark,
-    fontSize:FONT_SIZE_M
-  }
-})
+  emptyStateButtonText: {
+    textAlign: "center",
+    fontFamily: "Nunito-SemiBold",
+    color: colors.primaryDark,
+    fontSize: FONT_SIZE_M,
+  },
+});
 export default ConnectionList;
