b2e7579f1eed28f89188172249c4f6bd1992e5a6
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-08-30 22:34:06 -0400

    Update containerBottomPadding logic to account for messageInputMessage component and add ImageMessage component to render messages of type "image"

--- Frontend/src/components/chat/MessagesContainer.js
+++ Frontend/src/components/chat/MessagesContainer.js
@@ -1,20 +1,20 @@
 import {
   Animated,
   FlatList,
-  StyleSheet,
   Keyboard,
   Easing,
-  Text,
   ActivityIndicator,
 } from "react-native";
 import ReceivedMessage from "./ReceivedMessage";
 import SentMessage from "./SentMessage";
 import { useEffect, useRef } from "react";
-import { colors, FONT_SIZE_XL, FONT_SIZE_XXL } from "../../styles";
+import { colors, FONT_SIZE_XXL } from "../../styles";
 import ChatRevealing from "./ChatRevealing";
 import { useUser } from "../../contexts/UserContext";
 import ChatBeginning from "./ChatBeginning";
-const INITIAL_BOTTOM_PADDING = 50;
+import ImageMessage from "./ImageMessage";
+import { useState } from "react";
+const INITIAL_BOTTOM_PADDING = 40;
 function MessagesContainer({
   messages,
   isMatch,
@@ -26,20 +26,23 @@ function MessagesContainer({
   loadOlderMessages,
   isLoadingMore,
   setIsLoadingMore,
-  hideTime
+  hideTime,
+  imageHeight,
 }) {
   const flatListRef = useRef(null);
   const { user } = useUser();
   const containerBottomPadding = useRef(
     new Animated.Value(INITIAL_BOTTOM_PADDING)
   ).current;
+  const [prevImageHeight, setPrevImageHeight] = useState(0);
   useEffect(() => {
     const keyboardWillShowListener = Keyboard.addListener(
       "keyboardWillShow",
       (e) => {
         const { endCoordinates, duration } = e;
         Animated.timing(containerBottomPadding, {
-          toValue: endCoordinates.height + 40,
+          toValue:
+            endCoordinates.height + 20 + (imageHeight ? imageHeight + 20 : 0),
           duration: duration,
           easing: Easing.bezier(0, 0, 0.2, 1),
           useNativeDriver: false,
@@ -53,7 +56,7 @@ function MessagesContainer({
       (e) => {
         const { duration } = e;
         Animated.timing(containerBottomPadding, {
-          toValue: INITIAL_BOTTOM_PADDING,
+          toValue: imageHeight ? imageHeight + 60 : INITIAL_BOTTOM_PADDING,
           duration: duration,
           easing: Easing.bezier(0, 0, 0.2, 1),
           useNativeDriver: false,
@@ -65,7 +68,7 @@ function MessagesContainer({
       keyboardWillShowListener.remove();
       keyboardWillHideListener.remove();
     };
-  }, []);
+  }, [imageHeight]);
   useEffect(() => {
     if (isLoadingMore) {
       setIsLoadingMore(false);
@@ -73,7 +76,25 @@ function MessagesContainer({
       scrollToBottom();
     }
   }, [messages]);
-
+  useEffect(() => {
+    if (imageHeight !== 0) {
+      setPrevImageHeight(imageHeight);
+      Animated.timing(containerBottomPadding, {
+        toValue: imageHeight + 60,
+        duration: 0,
+        useNativeDriver: false,
+      }).start();
+    } else {
+      Animated.timing(containerBottomPadding, {
+        toValue: Math.max(
+          INITIAL_BOTTOM_PADDING,
+          containerBottomPadding.__getValue() - (prevImageHeight + 20)
+        ),
+        duration: 0,
+        useNativeDriver: false,
+      }).start();
+    }
+  }, [imageHeight]);
   const scrollToBottom = (animated = true) => {
     if (flatListRef.current && messages.length > 0) {
       flatListRef.current.scrollToOffset({ offset: 0, animated });
@@ -85,9 +106,27 @@ function MessagesContainer({
   const renderItem = ({ item, index }) => {
     const first = messages[index + 1]?.sender !== item.sender;
     const last = messages[index - 1]?.sender !== item.sender;
-
+    if (item.type === "image") {
+      return (
+        <ImageMessage
+          message={item}
+          isByUser={isByUser(item)}
+          first={first}
+          last={last}
+          isMatch={isMatch}
+          otherUser={otherUser}
+          lastItem={index === 0}
+        />
+      );
+    }
     return isByUser(item) ? (
-      <SentMessage key={item._id} text={item.text} first={first} last={last} />
+      <SentMessage
+        key={item._id}
+        text={item.text}
+        first={first}
+        last={last}
+        lastItem={index === 0}
+      />
     ) : (
       <ReceivedMessage
         key={item._id}
@@ -96,47 +135,49 @@ function MessagesContainer({
         last={last}
         isMatch={isMatch}
         otherUser={otherUser}
+        lastItem={index === 0}
       />
     );
   };
   return (
-      <Animated.View style={{ flex: 1, paddingBottom: containerBottomPadding }}>
-        <FlatList
-          ref={flatListRef}
-          data={messages}
-          renderItem={renderItem}
-          keyExtractor={(item) => item._id}
-          contentContainerStyle={{
-            flexGrow: 1,
-            justifyContent: "flex-end",
-            paddingRight: 25,
-          }}
-          inverted
-          onEndReached={() => loadOlderMessages()}
-          onEndReachedThreshold={0.1}
-          ListHeaderComponent={
-            isRevealing ? (
-              <ChatRevealing
-                conversation={conversation}
-                setConversation={setConversation}
-              />
-            ) : null
-          }
-          onScrollBeginDrag={hideTime}
-          ListFooterComponent={
-            isLoadingMore ? (
-              <ActivityIndicator
-                color={colors.primary}
-                size={FONT_SIZE_XXL}
-                style={{ marginVertical: 30 }}
-              />
-            ) : !hasMore ? (
-              <ChatBeginning user={otherUser} isMatch={isMatch} />
-            ) : null
-          }
-          showsVerticalScrollIndicator={false}
-        />
-      </Animated.View>
+    <Animated.View style={{ flex: 1, paddingBottom: containerBottomPadding }}>
+      <FlatList
+        ref={flatListRef}
+        data={messages}
+        renderItem={renderItem}
+        keyExtractor={(item) => item._id}
+        contentContainerStyle={{
+          flexGrow: 1,
+          justifyContent: "flex-end",
+          paddingRight: 25,
+        }}
+        keyboardShouldPersistTaps="handled"
+        inverted
+        onEndReached={() => loadOlderMessages()}
+        onEndReachedThreshold={0.1}
+        ListHeaderComponent={
+          isRevealing ? (
+            <ChatRevealing
+              conversation={conversation}
+              setConversation={setConversation}
+            />
+          ) : null
+        }
+        onScrollBeginDrag={hideTime}
+        ListFooterComponent={
+          isLoadingMore ? (
+            <ActivityIndicator
+              color={colors.primary}
+              size={FONT_SIZE_XXL}
+              style={{ marginVertical: 30 }}
+            />
+          ) : !hasMore ? (
+            <ChatBeginning user={otherUser} isMatch={isMatch} />
+          ) : null
+        }
+        showsVerticalScrollIndicator={false}
+      />
+    </Animated.View>
   );
 }
 export default MessagesContainer;
